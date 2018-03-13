922bf61b44cecf7a798ea004f8500f8b1bb3165b
Katie Wood <katiewood@Katies-MacBook-Pro.local>
2018-03-13 14:44:40 -0500

    coordinates

--- 01sketch.js
+++ 01sketch.js
@@ -1,29 +1,24 @@
 var index;
-//var mic;
-//var myVoice;
 var locationData;
 
-function preload() {
-  //soundFormats('mp3', 'ogg');
-  //ssss = loadSound('http://localhost:8000/assets/ssss.mp3');
-  //tremendous_velocity = loadSound('http://localhost:8000/assets/tremendous_velocity.mp3')
-  //tktk = loadSound('http://localhost:8000/assets/tktk.mp3')
+/*function preload() {
   locationData =  getCurrentPosition();
-  //myVoice = new p5.Speech();
-  }
+  }*/
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textFont("futura", 10);
 
+  getCurrentPosition(doThisOnLocation)
   if(geoCheck() == true){
-    //geolocation is available
+    print("hi (:")
   }else{
     text("to use this tool, please enable Location Services in your device's settings")//error getting geolocaion
   }
 
   index = 0;
-
+}
+/*
   print(locationData.latitude)
   print(locationData.longitude)
   print(locationData.accuracy)
@@ -31,68 +26,14 @@ function setup() {
   print(locationData.altitudeAccuracy)
   print(locationData.heading)
   print(locationData.speed)
-  //FFT = new p5.FFT();
-  //ssss.setVolume(0.3);
-  //tremendous_velocity.setVolume(0.3);
-  //tktk.setVolume(0.5);
-  //mic = new p5.AudioIn();
-  //mic.start();
-  //FFT.setInput(mic);
-  //ssss.playMode('untilDone');
-  //tremendous_velocity.playMode('untilDone');
-  //tktk.playMode('untilDone');
-  //var myRec = new p5.SpeechRec();
-  }
+  }*/
 
 function draw() {
-
   fill(255);
   translate(windowWidth/2, windowHeight/2);
   textAlign(CENTER);
-  
-  
+  function doThisOnLocation(position){
+    print("lat: " + position.latitude);
+    print("long: " + position.longitude);
 }
-  //myVoice.setPitch(1.6);
-  //myVoice.setRate(0.7);
-  //myVoice.setVolume(0.5);
-  //var mic_level = mic.getLevel();
-  //var spectrum = FFT.analyze()
-  //var bass_energy = FFT.getEnergy("bass")
-  //var mid_energy = FFT.getEnergy("mid")
-  //var hi_energy = FFT.getEnergy("highMid")
-  //var filter = 
-  //var waveform = FFT.waveform()
-
-
-  //text() takes 3 arguments: what text, where on x-axis, and where on y-axis
-  //text(mirrorSplit[index], random(-500, 500), random(-500,500));
-	//drawCount = drawCount + 1;
-
-//*FREQUENCY CONTROLLED PLAYBACK*//
-  /*if (bass_energy > 90){
-      //tktk.play();
-     myVoice.speak(mirrorSplit[index])
-      index++;
-     }
-   if (mid_energy > 10){
-      tremendous_velocity.play();
-     index++;
-     }
-   if (hi_energy > 10) {
-      ssss.play();
-    index++; 
-    }
-
-  if(index >= mirrorSplit.length) {
-    index = 0;
-    }
-  }*/
-
-/*function showResult() {
-  if(myRec.resultValue==true) {
-    background(192, 255, 192);
-    text(myRec.resultString, width/2, height/2);
-    console.log(myRec.resultString);
-    }
-  }*/
-//}
+}
