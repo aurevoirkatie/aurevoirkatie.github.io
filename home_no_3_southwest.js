7ea83fd7b8cf1fb78ffe14f5f1c1ab3510e39bc4
Katie Wood <kvwood@Katies-MacBook-Air.local>
2018-04-14 20:19:45 -0500

    added compass

--- home_no_3_southwest.js
+++ home_no_3_southwest.js
@@ -11,12 +11,12 @@ function success() {
 
 function preload() {
  soundFormats('mp3', 'ogg');
-    loss_no_3 = loadSound('assets/home_no_3_degradex2.mp3', success);
+    loss_no_3 = loadSound('assets/home_no_3_degradeOnly.mp3', success);
     original_no_3 = loadSound('assets/pinetrail.mp3');
 };
 
 var text_area;
-var content_home2;
+var content_home3;
 var compassHeading;
 var filterFreq;
 var timer;
@@ -25,14 +25,16 @@ function setup() {
     canvas = createCanvas(window.innerWidth, window.innerHeight);
     loss_no_3.setVolume(1.0);
     loss_no_3.playMode('untilDone');
-    content_home3 = [""];
+    original_no_3.setVolume(1.0);
+    original_no_3.playMode('untilDone');
+    content_home3 = ["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9"];
     text_area = document.getElementById("compass_content");
     i = .001;
     filterFreq = 100;
     filter = new p5.BandPass();
     loss_no_3.disconnect();
     loss_no_3.connect(filter);
-    filter.amp(1.0, 5, 0)
+    filter.amp(1.0, 20, 0)
     timer = 1;
 
   
@@ -49,148 +51,154 @@ function playbaqq() {
         filter.freq(freq);
         filter.res(i);
         loss_no_3.play();
+        if(original_no_3.isPlaying() == true){
+            original_no_3.pause();
+            };
+        text_area.innerHTML = "";
 }
        
 function draw(){
+//counters
     timer ++;
     var timeinseconds = timer/60
     console.log(timeinseconds);
 
+
     if (filterFreq <= 10000){
         filterFreq = filterFreq + 1.6;
-    } else {filterFreq = 100
+    } else {filterFreq = filterFreq - 1.6
     };
 
     if (timeinseconds == 180){
         if (i <= 1000){
-            i = i + 1;
-        } else {i = .001;
+            i++;
+        } else {i = i - 1;
         };
     }
 
-    playbaqq();
+ //text and sound triggers   
+    if(compassHeading >= 355 || compassHeading <=5){
+        text_area.innerHTML = auxText[0];
+        original_no_3.play();
+        if(loss_no_3.isPlaying() == true){
+            loss_no_3.pause();
+            };
+    };
 
+    if(compassHeading >= 5 && compassHeading <=31){
+        playbaqq();  
+    };
 
-    // if(compassHeading >= 90 && compasHeading <=180){
-    // };
+    if(compassHeading >= 31 && compassHeading <=41){
+        text_area.innerHTML = auxText[1];
+        original_no_3.play();
+        if(loss_no_3.isPlaying() == true){
+            loss_no_3.pause();
+            };
+    };
 
-// if(compassHeading >= 25 && compassHeading < 35){
-//             if(forNorth.isPlaying() == true){
-//             forNorth.pause();
-//             };
-//             if(forWest.isPlaying() == true){
-//             forWest.stop();
-//             };
-//         if (mouseIsPressed){
-//         text_area.innerHTML = auxText[2];
-//         image(jpg03, 20, 10, 700, 394);
-//         } else {
-//         text_area.innerHTML = " ";
-//         fill(0, 0, 255);
-//         noStroke();
-//         rect(19, 9, 701, 395);
-//         }; 
-// };
-//  //East by degrees - prose  
-// if(compassHeading >= 35 && compassHeading < 45){  
-//     if(forNorth.isPlaying() == true){
-//             forNorth.pause();
-//     };
-//     if(forWest.isPlaying() == true){
-//             forWest.pause();
-//     };
-//     if (mouseIsPressed){
-//         text_area.innerHTML = auxText[0];
-//         image(jpg01, 20, 10, 700, 394);
-//         } else {
-//         text_area.innerHTML =" ";
-//         fill(0, 0, 255);
-//         noStroke();
-//         rect(19, 9, 701, 395);
-//     };
-// };
-// if(compassHeading >= 45 && compassHeading < 55){
-//     if (mouseIsPressed){
-//         text_area.innerHTML = auxText[1];
-//         image(jpg02, 20, 10, 700, 394);
-//         } else {
-//         text_area.innerHTML = " ";
-//         fill(0, 0, 255);
-//         noStroke();
-//         rect(19, 9, 701, 395);
-//         };
-// };
-// if(compassHeading >= 55 && compassHeading < 65){
-//     if (mouseIsPressed){
-//         text_area.innerHTML = auxText[3];
-//         image(jpg04, 20, 10, 700, 394);
-//         } else {
-//         text_area.innerHTML = " ";
-//         fill(0, 0, 255);
-//         noStroke();
-//         rect(19, 9, 701, 395);
-//         };
-// };
-//     // if(compassHeading >= 65 && compassHeading < 75) {
-//     //           text_area.innerHTML = content_home5[0];  
-//    // };
-//     if(compassHeading >= 75 && compassHeading < 85) {
-//               text_area.innerHTML = content_home5[0];  
-//     };
-//     if(compassHeading >= 85 && compassHeading < 95) {
-//               text_area.innerHTML = content_home5[1];  
-//     };
-//     if(compassHeading >= 95 && compassHeading < 105) {
-//               text_area.innerHTML = content_home5[2];  
-//     };
-//     if(compassHeading >= 105 && compassHeading < 115) {
-//         text_area.innerHTML = content_home5[3];
-//     };
-//     if(compassHeading >= 115 && compassHeading < 125) {
-//         text_area.innerHTML = content_home5[4];
-//     };
-// //South poem by degrees
-//     if(compassHeading >= 125 && compassHeading < 135) {
-//         text_area.innerHTML = content_home5[5];    
-//     };
-//     if(compassHeading >= 135 && compassHeading < 145) {
-//         text_area.innerHTML = content_home5[6];
-//     };
-//     if(compassHeading >= 145 && compassHeading < 155) {
-//         text_area.innerHTML = content_home5[7];
-//     };
-//     if(compassHeading >= 155 && compassHeading < 165) {
-//         text_area.innerHTML = content_home5[8];
-//         if(forWest.isPlaying() == true){
-//             forWest.stop();
-//         };
-//         if(forNorth.isPlaying() == true){
-//             forNorth.pause();
-//         }; 
-//     };
+    if(compassHeading >= 41 && compassHeading <=67){
+        playbaqq();  
+    };
 
-// //West forWest.mp3(2minutes)
-//     if(compassHeading >= 235 && compassHeading < 335) {
-//         forWest.play();
-//         filterFreq++;
-//         if(filterFreq % 60 == 0){
-//             i++;
-//         }
-//         if(i >= forWest.duration()){
-//             forWest.stop();
-//         }
-//     if (mouseIsPressed){
-//         text_area.innerHTML = auxText[4];
-//         image(jpg04, 20, 10, 700, 394);
-//         } else {
-//         text_area.innerHTML = " ";
-//         fill(0, 0, 255);
-//         noStroke();
-//         rect(19, 9, 701, 395);
-//         };
-//         if(forNorth.isPlaying() == true){
-//             forNorth.pause();
-//         };
-//     };
+    if(compassHeading >= 67 && compassHeading <=77){
+        text_area.innerHTML = auxText[2];
+        original_no_3.play();
+        if(loss_no_3.isPlaying() == true){
+            loss_no_3.pause();
+            };
+    };
+
+    if(compassHeading >= 77 && compassHeading <=103){
+        playbaqq();  
+    };
+
+    if(compassHeading >= 103 && compassHeading <=113){
+        text_area.innerHTML = auxText[3];
+        original_no_3.play();
+        if(loss_no_3.isPlaying() == true){
+            loss_no_3.pause();
+            };
+    };
+
+    if(compassHeading >= 113 && compassHeading <=139){
+        playbaqq();  
+    };
+
+    if(compassHeading >= 139 && compassHeading <=149){
+        text_area.innerHTML = auxText[4];
+        original_no_3.play();
+        if(loss_no_3.isPlaying() == true){
+            loss_no_3.pause();
+            };
+    };
+
+    if(compassHeading >= 149 && compassHeading <=175){
+        playbaqq();  
+    };
+
+    if(compassHeading >= 175 && compassHeading <=185){
+        text_area.innerHTML = auxText[4];
+        original_no_3.play();
+        if(loss_no_3.isPlaying() == true){
+            loss_no_3.pause();
+            };
+    };
+
+    if(compassHeading >= 185 && compassHeading <=211){
+        playbaqq();  
+    };
+
+    if(compassHeading >= 211 && compassHeading <=221){
+        text_area.innerHTML = auxText[5];
+        original_no_3.play();
+        if(loss_no_3.isPlaying() == true){
+            loss_no_3.pause();
+            };
+    };
+
+    if(compassHeading >= 221 && compassHeading <=247){
+        playbaqq();  
+    };
+
+    if(compassHeading >= 247 && compassHeading <=257){
+        text_area.innerHTML = auxText[6];
+        original_no_3.play();
+        if(loss_no_3.isPlaying() == true){
+            loss_no_3.pause();
+            };
+    };
+
+    if(compassHeading >= 257 && compassHeading <=283){
+        playbaqq();  
+    };
+
+    if(compassHeading >= 283 && compassHeading <=288){
+        text_area.innerHTML = auxText[7];
+        original_no_3.play();
+        if(loss_no_3.isPlaying() == true){
+            loss_no_3.pause();
+            };
+    };
+
+    if(compassHeading >= 288 && compassHeading <=319){
+        playbaqq();  
+    };
+
+    if(compassHeading >=319 && compassHeading <=329){
+        text_area.innerHTML = auxText[8];
+        original_no_3.play();
+        if(loss_no_3.isPlaying() == true){
+            loss_no_3.pause();
+            };
+    };
+
+    if (timeinseconds >= loss_no_3.duration()){
+        loss_no_3.stop();
+    };
+
+    if (timeinseconds >= original_no_3.duration()){
+        original_no_3.stop();
+    };
 };
 
